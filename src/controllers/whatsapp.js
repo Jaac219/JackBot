e533c3da509bc8f8baaf7c8a0d85136774ae0d3f
Jaac219 <jaac219@gmail.com>
2023-07-03 23:55:05 -0500

    Pong configurations

--- src/controllers/whatsapp.js
+++ src/controllers/whatsapp.js
@@ -1,4 +1,6 @@
+const { text } = require('express')
 const fs = require('fs')
+const { sendMessage } = require('../services/whatsapp')
 // const myConsole = new console.Console(fs.createReadStream('./logs.txt'))
 
 const verifyToken = (req, res) => {
@@ -21,13 +23,39 @@ const receibeMessage = (req, res) => {
     const entry = req.body['entry'][0]
     const { value: { messages } } = entry['changes'][0]
 
-    console.log(messages)
+    if (messages !== 'undefined') {
+      const txt = getText(messages[0])
+      const number = messages[0]['from']
+
+      sendMessage(`El usuario dijo: ${txt}`, number)
+    }
 
     res.status(400).send('EVENT_RECEIVED')
   } catch (e) {
     console.log(e);
     res.status(400).send('EVENT_RECEIVED')
   }
+}
+
+const getText = (messages) => {
+  const type = messages['type']
+  let txt = ''
+  switch (type) {
+    case 'text':
+        txt = messages['text']['body']
+      break;
+    case 'interactive':
+        const interactiveObj = messages['interactive']
+        const typeInteractive = interactiveObj['type']
+
+        txt = interactiveObj[typeInteractive]['title']
+      break;
+    default:
+        console.log('Sin mensaje');
+      break;
+  }
+  console.log('Txt entrante ----> ', txt)
+  return txt
 }
 
 module.exports = {
