ae3b9ae5d2139a4aa9d912cd167530c1d81c68e4
Jaac219 <jaac219@gmail.com>
2023-07-03 22:32:01 -0500

    Logica para que whatsapp se sincronice con mi app

--- src/controllers/whatsapp.js
+++ src/controllers/whatsapp.js
@@ -1,5 +1,14 @@
 const verifyToken = (req, res) => {
-  res.send('verify token')
+  try {
+    const accessToken = "AJSDLFJASLDFASJDFLAKSFD"
+    const token = req.query['hub.verify_token']
+    const challenge = req.body['hub.challenge']
+
+    if(challenge && token === accessToken) res.send(challenge)
+    else res.status(400).send()
+  } catch (e) {
+    res.status(400).send()
+  }
 }
 
 const receibeMessage = (req, res) => {
